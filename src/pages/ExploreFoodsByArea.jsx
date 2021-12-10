193ffe5a4a7d2f588e9a4c1319d50f7a5b1438db
João Felipe P <jf.pelliccione@gmail.com>
2021-12-10 15:22:33 -0300

    Menu Inferior - Reqs 19 a 24 realizados!

--- src/pages/ExploreFoodsByArea.jsx
+++ src/pages/ExploreFoodsByArea.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function ExploreFoodsByArea() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
