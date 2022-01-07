ffc4d1dd879276e2b46e075ffc3fe84836d2d343
João Felipe P <jf.pelliccione@gmail.com>
2022-01-07 19:19:11 -0300

    Recuperando o tempo perdido - 2.0!

--- src/components/RecipesMadeComponents/RecipesMadeFilters.jsx
+++ src/components/RecipesMadeComponents/RecipesMadeFilters.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function RecipesMadeFilters() {
+function RecipesMadeFilters({ setSelectFilter }) {
   return (
     <div>
       <button
-        type="button"
         data-testid="filter-by-all-btn"
+        type="button"
+        onClick={ () => setSelectFilter('All') }
       >
         All
       </button>
       <button
-        type="button"
         data-testid="filter-by-food-btn"
+        type="button"
+        onClick={ () => setSelectFilter('Food') }
       >
         Food
       </button>
       <button
-        type="button"
         data-testid="filter-by-drink-btn"
+        type="button"
+        onClick={ () => setSelectFilter('Drinks') }
       >
-        Drink
+        Drinks
       </button>
     </div>
   );
 }
+
+RecipesMadeFilters.propTypes = {
+  setSelectFilter: PropTypes.func.isRequired,
+};
 
 export default RecipesMadeFilters;
--- src/pages/RecipesMade.jsx
+++ src/pages/RecipesMade.jsx
@@ -6,28 +6,61 @@ import MealMadeCard from '../components/RecipesMadeComponents/MealMadeCard';
 import DrinkMadeCard from '../components/RecipesMadeComponents/DrinkMadeCard';
 
 function RecipesMade() {
-  const doneRecipesArray = useSelector((state) => state.user.doneRecipes);
+  const inProgressRecipesObj = useSelector((state) => state.user.inProgressRecipes);
+
+  const doneRecipesArrayFromRedux = useSelector((state) => state.user.doneRecipes);
+  const [doneRecipesArray, setDoneRecipesArray] = React
+    .useState(doneRecipesArrayFromRedux);
+
   const isThereSomeMealRecipe = doneRecipesArray.some(({ type }) => type === 'comida');
   const isThereSomeDrinkRecipe = doneRecipesArray.some(({ type }) => type === 'bebida');
 
-  const inProgressRecipesObj = useSelector((state) => state.user.inProgressRecipes);
+  const [selectFilter, setSelectFilter] = React.useState('All');
 
   // ==========================IN-PROGRESS-RECIPES-LS // DONE-RECIPES-LS=======================
   React.useEffect(() => {
     localStorage.setItem('doneRecipes', JSON.stringify(doneRecipesArray));
     localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipesObj));
   }, [doneRecipesArray]);
-  // =============================================================================
+  // =========================================================================================
+
+  // ==========================LÓGICA-FILTROS================================================
+  const onSelectFilter = () => {
+    if (selectFilter === 'All') {
+      setDoneRecipesArray(doneRecipesArrayFromRedux);
+    }
+    if (selectFilter === 'Food') {
+      setDoneRecipesArray(doneRecipesArrayFromRedux
+        .filter(({ type }) => type === 'comida'));
+    }
+    if (selectFilter === 'Drinks') {
+      setDoneRecipesArray(doneRecipesArrayFromRedux
+        .filter(({ type }) => type === 'bebida'));
+    }
+  };
+
+  React.useEffect(() => {
+    onSelectFilter();
+  }, [selectFilter]);
+  // =========================================================================================
 
   return (
     <div>
       <header>
         <Header />
-        <RecipesMadeFilters />
+        <RecipesMadeFilters
+          setSelectFilter={ setSelectFilter }
+        />
       </header>
       <section>
-        {isThereSomeMealRecipe && <MealMadeCard />}
-        {isThereSomeDrinkRecipe && <DrinkMadeCard />}
+        {isThereSomeMealRecipe
+        && <MealMadeCard
+          doneRecipesArray={ doneRecipesArray }
+        />}
+        {isThereSomeDrinkRecipe
+        && <DrinkMadeCard
+          doneRecipesArray={ doneRecipesArray }
+        />}
       </section>
     </div>
   );
